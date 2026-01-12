9ee9b8f872860e37d7c7fd46eeed50c609414324
shiva-karan-k <lucky3aeon@yahoo.com>
2026-01-12 22:56:35 +0530

    fix: Remove incorrect fish placement from week 6 blue rectangle
    - Fish should only appear on the RIGHT side in the title section
    - Removed blue rectangle with fish below video thumbnail
    - Week 6 now matches the correct reference design

--- src/app/weeks/week-6/page.tsx
+++ src/app/weeks/week-6/page.tsx
@@ -95,12 +95,6 @@ export default function Week6Page() {
 
             <div className="video-section-container">
               <div className="video-thumbnail-wrapper">
-                <div className="blue-rectangle-below-thumbnail">
-                  <div className="blue-rectangle"></div>
-                  <div className="bear-on-blue-block">
-                    <img src="/assets/images/w1/fish.png" alt="Fish" className="bear-sign-asset" />
-                  </div>
-                </div>
                 <div className="lock-overlay">
                   <div className="lock-icon"></div>
                 </div>
